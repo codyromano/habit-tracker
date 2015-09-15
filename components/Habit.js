42bf38de44de0d6b207612d59d73f326cbcce420
Cody Romano <codyromano@gmail.com>
2015-09-14 19:09:31 -0700

    Improve style, touch responsiveness

--- components/Habit.js
+++ components/Habit.js
@@ -7,7 +7,8 @@
     getInitialState: function() {
       return {
         timeLeftAsPercentage: 0,
-        actionMenuHidden: true
+        actionMenuHidden: true,
+        warned: false
       };
     },
 
@@ -30,6 +31,12 @@
       }
 
       this.setState({'timeLeftAsPercentage': timeLeftAsPercentage});
+
+      if (timeLeftAsPercentage > 25 && timeLeftAsPercentage < 50 && !this.state.warned) {
+        this.setState({warned: true}); 
+        PubSub.publish('messageAdded', "Warning: You need to" + this.props.title + 
+          " ASAP or you'll be demoted to a lower level.", 4000); 
+      }
     },
 
     render: function() {
@@ -47,7 +54,7 @@
         'habit-action-icon-expanded' : !_self.state.actionMenuHidden
       });
 
-      return <div className="habit" onClick={this.toggleActionMenu}>
+      return <div className="habit noselect" onClick={this.toggleActionMenu}>
         <div className={progressClasses} style={progressStyle}></div>
 
         <div className="habit-level">Lvl {this.props.level}</div>
