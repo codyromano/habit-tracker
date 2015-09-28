df7daf5906ba046f0c834de2ce2daf2494e99785
Cody Romano <romanocr@amazon.com>
2015-09-28 14:01:26 -0700

    Fix ELB 502 error

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ var express      =  require('express'),
 
 var habit        =  new Habit();
 
+var rootDir = path.dirname(require.main.filename);
+var pathToConfig = './appConfig.json';
+
 // General config including params needed by AWS
-var config = fs.readFileSync('./appConfig.json', 'utf8');
+var config = fs.readFileSync(pathToConfig, 'utf8');
 config = JSON.parse(config);
 
 // AWS and DynamoDB configuration
@@ -23,12 +26,8 @@ var db = new AWS.DynamoDB({region: config.AWS_REGION});
 // Configure express server
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-var rootDir = path.dirname(require.main.filename);
 
 // Designate 'public' as a static directory
 app.use(express.static('public'));
@@ -40,5 +39,4 @@ app.get('/', function(req, res) {
 
 app.post('/api/habits/', habit.saveAll.bind(undefined, config, db));
 
-app.listen(app.get('port'));
-console.log('Listening on port %s', app.get('port'));
+module.exports = app;
