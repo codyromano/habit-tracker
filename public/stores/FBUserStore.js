1e922416a8e8a36239f87c3e55070e8366f8883c
Cody Romano <codyromano@gmail.com>
2015-10-04 01:55:51 -0700

    Prevent FB SDK race condition

--- public/stores/FBUserStore.js
+++ public/stores/FBUserStore.js
@@ -53,4 +53,13 @@
     });
   };
 
+  // Facebook SDK
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 })(window, PubSub);
