48d12b76f6e3324053849dd68089988862796dbb
Cody Romano <romanocr@amazon.com>
2015-10-09 16:00:56 -0700

    Simplify JS minify in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,10 @@ var gulp     = require('gulp'),
 app.set('port', process.env.PORT || 8081);
 
 gulp.task('minify', function () {
-
-  /**
-  * @todo Process and concatenate JSX files as well as plain JS 
-  */
-   return gulp.src([
-        'public/publicConfig.js',
-
-        /* TODO: Remove jQuery. I'm only using it for AJAX calls in two 
-        places; the library is unnecessarily expensive */
-        'public/thirdparty/jquery-2.1.4.min.js',
-        'public/thirdparty/fastclick.js',
-        'public/thirdparty/es5-shim.min.js',
-        'public/thirdparty/es5-sham.min.js',
-        'public/thirdparty/console-polyfill.js',
-        'public/thirdparty/react.js',
-        'public/thirdparty/JSXTransformer.js',
-        'public/publicConfig.js',
-        'public/utils/U.js',
-        'public/utils/PubSub.js',
-        'public/stores/UserStore.js',
-        'public/stores/FBUserStore.js',
-        'public/stores/HabitStore.js',
-        'public/stores/MessageStore.js'
-        ])
-      .pipe(concat('all-non-jsx.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./public/'));
+  return gulp.src('public/*.js')
+    .pipe(concat('all-non-jsx.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('startServer', function() {
